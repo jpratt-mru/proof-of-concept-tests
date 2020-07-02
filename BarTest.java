4daa0a7496276b4885ea85335a39a4c647505234
Jordan Pratt <jpratt@mtroyal.ca>
2020-07-02 15:18:16 -0600

    Create BarTest.java

---
+++ BarTest.java
@@ -0,0 +1,19 @@
+package drill;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.rice.autograder.annotations.Grade;
+import edu.rice.autograder.annotations.GradeTopic;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("tests for Bar")
+@GradeTopic(project = "Drill Template", topic = "Things")
+class FooTest {
+  @Test
+  @DisplayName("it works")
+  @Grade(project = "Drill Template", topic = "Things", points = 1.0)
+  void test() {
+    assertEquals("foo", new Foo().ping(2));
+  }
+}
